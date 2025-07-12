c758748ce5ace4172d2ae5f81169f7ca3eda8e61
Fevzi Ketrez <fevzi.ketrez@gmail.com>
2025-07-12 20:10:02 +0300

    main.c test

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fketrez <fketrez@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 17:38:11 by fketrez           #+#    #+#             */
-/*   Updated: 2025/07/12 19:50:15 by fketrez          ###   ########.fr       */
+/*   Updated: 2025/07/12 20:09:38 by fketrez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int	main(void)
 {
 	int a = open("test.txt", O_RDWR | O_CREAT | O_APPEND, 0644);
 	char *to_print = "test1, test2, test3";
+	char *temp;
 
 	int i = 0;
 	int j;
@@ -53,9 +54,18 @@ int	main(void)
 	i = 0;
 	while (mystr[1 + i])
 	{
-		mystr[0] = ft_strjoin(mystr[0], "\n");
-		mystr[0] = ft_strjoin(mystr[0], mystr[1 + i]);
+		temp = ft_strjoin(mystr[0], "\n");
+		mystr[0] = ft_strjoin(temp, mystr[1 + i]);
+		free(temp);
+		free(mystr[1 + i]);
 		i++;
 	}
 	ft_putstr_fd(mystr[0], a);
+	i = 0;
+	while (mystr[i])
+	{
+		free(mystr[i]);
+		i++;
+	}
+	free(mystr);
 }
